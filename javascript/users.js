e6a91d9cff1fdcf081568b67045cc8fa4fceeef1
omar saade <omarsaadeh11@gmail.com>
2022-03-24 14:39:13 +0200

    Chat Web Application using PHP with MySQL & JavaScript

--- javascript/users.js
+++ javascript/users.js
@@ -3,7 +3,7 @@ const searchBtn = document.querySelector(".users .search button");//button
 
 
 searchBtn.onclick = () => {
-    searchBar.classList.toggle("active");//marra hik marra hik
+    searchBar.classList.toggle("active");//marra hik marra hik 
     searchBar.focus();
     searchBtn.classList.toggle("active");
 }
